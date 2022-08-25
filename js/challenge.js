f8d69d9d50a923b833390bbef11f211bbb3b7074
Tevin-Mugairwa <tevin.mugairwa@student.moringaschool.com>
2022-08-25 22:26:44 +0300

    complete

--- js/challenge.js
+++ js/challenge.js
@@ -1,58 +1,67 @@
-"use strict";function _toConsumableArray(a)
-{
-    if(Array.isArray(a))
-    {
-        for(var b=0,c=Array(a.length);b<a.length;b++)c[b]=a[b];
-        return c
+function header(){
+  return setInterval(() => {
+    let header1 = document.querySelector("#counter")
+    addInteg =parseInt(header1.innerText);
+    header1.innerText=addInteg+1;
+  },1000)
+};
+const minus = document.querySelector("#minus");
+minus.addEventListener("click",() =>{
+    let header1 = document.querySelector("#counter")
+    addingInteg = parseInt(header1.innerText);
+    header1.innerText= addingInteg-1 
+
+})
+console.log(minus);
+
+const plus=document.getElementById("plus");
+plus.addEventListener("click",() => {
+    let header1 = document.querySelector("#counter")
+    addingInteg = parseInt(header1.innerText);
+    header1.innerText= addingInteg+1
+})
+console.log(plus);
+
+let totalLikes = {};
+heart = document.querySelector("#heart");
+heart.addEventListener("click",(event) => {
+    let header1 = document.querySelector("#counter")
+    addingInt = parseInt(header1.innerText);
+    totalLikes[addingInteg] ? totalLikes[addingInteg] += 1 : totalLikes[addingInt] = 1
+    let ul =document.querySelector(".likes")
+    event.preventDefault()
+    if (document.getElementById(`like-${addingInteg}`)){
+        document.getElementById(`like-${addingInteg}`).innerText =`${addingInteg} has been liked 1  times`    
     }
-    return Array.from(a)
-}
-    var playing=!0,timer=function()
-    {
-        return setInterval(function()
-        {var a=document.getElementById("counter"),b=parseInt(a.innerText);
-        a.innerText=b+1},1e3)},interval=timer(),
-        minus=document.getElementById("minus"),
-        plus=document.getElementById("plus"),
-        heart=document.getElementById("heart"),
-        pause=document.getElementById("pause"),
-        restart=document.getElementById("restart")
-        commentForm=document.getElementsByTagName("form")[0];
-        minus.addEventListener("click",function()
-        {
-            var a=document.getElementById("counter"),
-            b=parseInt(a.innerText);a.innerText=b-1
-        }), 
-plus.addEventListener("click",function()
-{
-    var a=document.getElementById("counter"),b=parseInt(a.innerText);
-     a.innerText=b+1
-}),
-   heart.addEventListener("click",function()
-{
-    var a=document.getElementById("counter"),
-    b=parseInt(a.innerText),c=document.querySelector(".likes"),d=void 0;
-      if([].concat(_toConsumableArray(c.children)).map(function(a)
-       {
-          return parseInt(a.dataset.num)
-       })
-        .includes(b)){d=document.querySelector('[data-num="'+b+'"]');
-           var e=parseInt(d.children[0].innerText);
-             d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}
-               else(d=document.createElement("li")).setAttribute("data-num",b),d.innerHTML=b+" has been liked <span>1</span> time",c.appendChild(d)}),
-               pause.addEventListener("click",function()
-            
-             {
-                playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),this.innerText="pause"),[].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a)
-                {
-                    "pause"!==a.id&&(a.disabled=!playing)
-                })
-            })
-                    
-                    ,commentForm.addEventListener("submit",function(a)
-               {
-                        a.preventDefault();var b=this.children[0],c=b.value;b.value="";
-                           var d=document.querySelector(".comments"),
-                             e=document.createElement("p");
-                               e.innerText=c,d.appendChild(e)
-                });
+    else {
+        let list = document.createElement("li")
+        list.id = `like-${addingInteg}`
+        list.innerText = `${addingInteg} has been like 1  time`
+        ul.appendChild(list)
+    }  
+})
+console.log(heart)
+
+pause = document.querySelector("#pause");
+pause.addEventListener("click",() => {
+    pause.innerText = (pause.innerText === "pause") ? "resume" : "pause";
+    btn = document.querySelector("#pause")
+    header1 =document.querySelector("#counter")
+    minusbtn = document.querySelector('#minus')
+    submitbtn = document.querySelector('#submit')
+    heartbtn = document.querySelector('#heart')
+    let arrayOfButtons = [btn,header1, minusbtn, submitbtn, heartbtn];
+    arrayOfButtons.forEach((button) => {"pause"!== button.id && (button.disabled =! pause)})
+
+})
+console.log(pause)
+
+commentForm = document.querySelector("#comment-form");
+commentForm.addEventListener("submit",(event) =>{
+    event.preventDefault()
+    let comment = document.querySelector('#comment-input').value
+    let commentDiv = document.querySelector("#list")
+    let newPara = document.createElement("p")
+    commentDiv.appendChild(newPara) 
+    newPara.innerText = comment
+})
